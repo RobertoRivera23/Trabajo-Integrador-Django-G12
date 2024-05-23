9f3cb48b1d2588aa2ae6ddbe7fd21c7bc2e01d90
Romherre <98140472+Romherre@users.noreply.github.com>
2024-05-23 09:19:14 -0300

    Se actuaiza rama

--- FutbolConnect/FutbolConnect/web/urls.py
+++ FutbolConnect/FutbolConnect/web/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # solo agregue el > a la linea 
-    path('saludar/<str:nombre>/', views.saludar, name='saludar'),
+    path('saludar/<str:nombre>', views.saludar, name='saludar'),
     path('listado_jugadores', views.listado_jugadores, name='listado_jugadores'),
     
 ]
